a006ec18fc676e64e3471847723bb73b9436e66c
Michael Bestas <mkbestas@lineageos.org>
2026-01-05 02:39:35 +0200

    treewide: Fix errors due to assumption of CONFIG_DRM_MSM always being set
    CONFIG_DRM_MSM should not be set in defconfig since it builds the
    upstream gpu driver. It is set in techpack/display, however various
    drivers depend on it for DRM notifier.
    Mimic what sm8350 kernel does to work around the issue.
    
    Change-Id: Iae02fed3822681fa8f54940d1a6d8cd3f830510d

--- drivers/input/touchscreen/oplus_touchscreen/Focal/ft3658u/focal_drivers_ft3658u.c
+++ drivers/input/touchscreen/oplus_touchscreen/Focal/ft3658u/focal_drivers_ft3658u.c
@@ -2023,7 +2023,7 @@ static struct of_device_id tp_match_table[] = {
 };
 
 static const struct dev_pm_ops tp_pm_ops = {
-#if defined(CONFIG_FB) || defined(CONFIG_DRM_MSM)
+#if defined(CONFIG_FB) || defined(CONFIG_DRM_MSM) || defined(CONFIG_DRM_OPLUS_NOTIFY)
     .suspend = fts_i2c_suspend,
     .resume = fts_i2c_resume,
 #endif
